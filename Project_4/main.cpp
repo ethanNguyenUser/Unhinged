ee5e9e48651ad425eab9a73ede3f1ccfe9bcf76b
Ethan Nguyen <ethan.n.email@gmail.com>
2022-03-07 19:21:31 -0800

    Initial Commit

---
+++ Project_4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project_4
+//
+//  Created by Ethan Nguyen on 3/7/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
